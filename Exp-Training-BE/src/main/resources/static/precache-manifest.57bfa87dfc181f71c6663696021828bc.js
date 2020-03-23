4b9151d0399e5a1c9ce80cc7d6fe5b2f73a1d836
jeetsaha5338 <jeetsaha093@gmail.com>
2020-03-23 19:11:36 +0530

    React - npm build

---
+++ Exp-Training-BE/src/main/resources/static/precache-manifest.57bfa87dfc181f71c6663696021828bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199b31fd89a74b343036910ff5a62bed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2084c13ea21fdcfa365",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0f5f88a78c568477831e",
+    "url": "/static/css/main.df2ea655.chunk.css"
+  },
+  {
+    "revision": "a2084c13ea21fdcfa365",
+    "url": "/static/js/2.a8aee673.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a8aee673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5f88a78c568477831e",
+    "url": "/static/js/main.19ad70e5.chunk.js"
+  },
+  {
+    "revision": "c0ce0821964dd6bcbff9",
+    "url": "/static/js/runtime-main.043ac302.js"
+  }
+]);
